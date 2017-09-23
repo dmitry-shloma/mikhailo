c7437e3d30545947128e8508a2b38b437421a6a1
Dmitry Shloma <dmitry.shloma@mail.ru>
2017-09-24 01:35:46 +0600

    Добавлено описание

--- src/firmwares/lcdtext/randhelper.h
+++ src/firmwares/lcdtext/randhelper.h
@@ -4,11 +4,10 @@
 #include <stdlib.h>
 
 /**
- * @brief init_rand
- * @param seed
+ * @brief init_rand function for initialization pseudo-random number generator algorithm
+ * @param seed integer value to be used as seed by the algorithm
  */
-void init_rand(unsigned int seed)
-{
+void init_rand(unsigned int seed) {
     static bool is_init = false;
     if (!is_init) {
         srand(seed);
@@ -17,36 +16,32 @@ void init_rand(unsigned int seed)
 }
 
 /**
- * @brief rrand
- * @param min
- * @param max
- * @return
+ * @brief rrand function that returns a random integer number in the range
+ * @param min start of the range
+ * @param max end of range
+ * @return random integer number in the range
  */
-int rrand(int min, int max)
-{
+int rrand(int min, int max) {
     return min + rand() % (max - min + 1);
 }
 
 /**
- * @brief brand
- * @return
+ * @brief brand function that returns a random "true" or "false"
+ * @return a random "true" or "false"
  */
-bool brand()
-{
+bool brand() {
     return rrand(0, 1);
 }
 
 /**
- * @brief frrand
- * @param min
- * @param max
- * @return
+ * @brief rrand function that returns a random float number in the range
+ * @param min start of the range
+ * @param max end of range
+ * @return random float number in the range
  */
-float frrand(float min, float max)
-{
+float frrand(float min, float max) {
     return min + (rand() / (RAND_MAX / (max - min)));
 }
 
 #endif // RANDHELPER_H
-
 
