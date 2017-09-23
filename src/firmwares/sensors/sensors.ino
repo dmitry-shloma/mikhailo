417720557256e6f65c4921253b05ae8628df3974
Dmitry Shloma <dmitry.shloma@mail.ru>
2017-09-24 01:55:47 +0600

    1. Правка в связи с изменившимися вспомогательными файлами 2. Небольшая оптимизация

--- src/firmwares/sensors/sensors.ino
+++ src/firmwares/sensors/sensors.ino
@@ -7,26 +7,34 @@
 #define HIH4000_PIN 0
 #define ONE_SEC 1000
 
+// LCD size's
+#define LCD_COLS 24
+#define LCD_ROWS 2
+
 unsigned char addrv[3][8] = {{0}};
 uint8_t addrc = 0;
 
 void setup()
 {
-    lcd_init();
+    lcd_init(LCD_COLS, LCD_ROWS);
     onewire_enum_avaliable_devices(&addrc, addrv);
 }
 
 void loop()
 {
     for (int i = 0; i < addrc; ++i) {
-        char str[17] = "";
+        char str[19] = "";  // 0...15 byte for addr, 16 byte - ':', 17 byte - ' ', 18 byte - '\0'
         onewire_addr_to_str(addrv[i], str);
-        lcd_out_text(str, 0, 0, true);
+        str[16] = ':';
+        str[17] = ' ';
+        str[18] = '\0';
+                
+        lcd_clear();
         float t = onewire_get_temperature(addrv[i], C);
-        lcd_out_value(": ", t, 4, 1, T, 16, 0, false);
+        lcd_out_value(str, t, 4, 1, "DEGREE_C", 0, 0);
         
         float rh = get_humidity(HIH4000_PIN);
-        lcd_out_value("RH: ", rh, 4, 1, RH, 0, 1, false);
+        lcd_out_value("RH: ", rh, 4, 1, "RH", 0, 1);
         delay(2 * ONE_SEC);
     }
         
