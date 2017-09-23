b2b8d3da5c34725bfa16d729dc0621a3d825ce2e
Dmitry Shloma <dmitry.shloma@mail.ru>
2017-09-24 01:35:06 +0600

    Исправлено в соответствии с изменившимися вспомогательными файлами

--- src/firmwares/lcdtext/lcdtext.ino
+++ src/firmwares/lcdtext/lcdtext.ino
@@ -6,13 +6,18 @@
 #define ONE_SEC 1000
 #define UNCONNECTED_PIN 5
 
+// LCD size's
+#define LCD_COLS 24
+#define LCD_ROWS 2
+
 void setup()
 {
     init_rand(analogRead(UNCONNECTED_PIN));
+    lcd_init(LCD_COLS, LCD_ROWS);
     
-    lcd_init();
     const char *banner = "AQUARIUM";
-    lcd_out_text(banner, (24 - strlen(banner)) / 2, 0, true);
+    lcd_clear();
+    lcd_out_text(banner, (24 - strlen(banner)) / 2, 0);
     delay(5 * ONE_SEC);
 }
 
@@ -23,12 +28,13 @@ void loop()
         t[i] = frrand(0.0, 99.0);
     };
 
-    lcd_out_value("1: ", t[0], 2, 0, T, 0, 0, true);
-    lcd_out_value("2: ", t[1], 2, 0, T, 8, 0, false);
-    lcd_out_value("3: ", t[2], 2, 0, T, 16, 0, false);
-    lcd_out_value("4: ", t[3], 2, 0, RH, 0, 1, false);
-    lcd_out_value("5: ", t[4], 2, 0, RH, 8, 1, false);
-    lcd_out_value("6: ", t[5], 2, 0, V, 16, 1, false);
+    lcd_clear();
+    lcd_out_value("1: ", t[0], 2, 0, "DEGREE_C", 0, 0);
+    lcd_out_value("2: ", t[1], 2, 0, "DEGREE_F", 8, 0);
+    lcd_out_value("3: ", t[2], 2, 0, "RH", 16, 0);
+    lcd_out_value("4: ", t[3], 2, 0, "RH", 0, 1);
+    lcd_out_value("5: ", t[4], 2, 0, "DEGREE", 8, 1);
+    lcd_out_value("6: ", t[5], 2, 0, "NONE", 16, 1);
     
     delay(ONE_SEC);
 }
